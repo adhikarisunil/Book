13f92ec95a5e97260b98ca782a54601ed1f55046
sunil <adhikarisunil339@gmail.com>
2023-08-01 18:38:27 +0545

    update

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import Book, Nobel, Poem
 from .serializers import BookSerializer, NobelSerializer, PoemSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
@@ -11,21 +11,21 @@ from rest_framework import filters
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['author']
 
 class  NobelViewSet(viewsets.ModelViewSet):
     queryset = Nobel.objects.all()
     serializer_class = NobelSerializer   
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['author']  
 
 class  PoemViewSet(viewsets.ModelViewSet):
     queryset = Poem.objects.all()
     serializer_class = PoemSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['author']
 
