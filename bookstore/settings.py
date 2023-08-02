8162853d4453c02dfd9462a417c53dcd3cc43efb
sunil <adhikarisunil339@gmail.com>
2023-08-02 16:55:10 +0545

    update

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -63,7 +63,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
-    # ]
+    # ],
+ 
+    # 'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S.%fZ",
+
 }
 
 ROOT_URLCONF = 'bookstore.urls'
